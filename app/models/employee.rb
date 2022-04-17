b66ee8c9614b2f3f2e32b6baddae77996c9acd2b
timon <k.poloczek7@gmail.com>
2022-04-17 23:04:39 +0200

    fix associations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,5 +4,6 @@ class Employee < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :kudos, dependent: :destroy
+  has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
+  has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
 end
