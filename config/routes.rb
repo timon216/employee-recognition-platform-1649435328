25b1c28fda2f0b8ea1b1aba832ff2d37c948040e
timon <k.poloczek7@gmail.com>
2022-04-21 18:53:31 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admin_users
+  devise_for :admin_users, path: "admin"
+  devise_for :employees, path: "employee"
   resources :kudos
-  devise_for :employees
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "kudos#index"
