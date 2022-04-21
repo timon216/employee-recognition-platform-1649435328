c421e7b5da0053d93291df35a4d2529d194c78eb
timon <k.poloczek7@gmail.com>
2022-04-21 19:52:18 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admin_users, path: "admin"
-  devise_for :employees, path: "employee"
+  devise_for :admin_users, path: "admin", controllers: {
+    sessions: "admin_users/sessions"
+  }
+  devise_for :employees, path: "employee", controllers: {
+    sessions: "employees/sessions"
+  }
   resources :kudos
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
