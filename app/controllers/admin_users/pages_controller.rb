136f08d4c1bf4fbd2e15b8c063f823dc26f8190f
timon <k.poloczek7@gmail.com>
2022-04-25 21:49:32 +0200

    add rubocop corrections

--- app/controllers/admin_users/pages_controller.rb
+++ app/controllers/admin_users/pages_controller.rb
@@ -1,4 +1,5 @@
-class AdminUsers::PagesController < ApplicationController
-  def dashboard
+module AdminUsers
+  class PagesController < ApplicationController
+    def dashboard; end
   end
 end
--- spec/system/admin_users_spec.rb
+++ spec/system/admin_users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "AdminUsers", type: :system do
+RSpec.describe 'AdminUsers', type: :system do
   before do
     driven_by(:rack_test)
   end
