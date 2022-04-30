2e90dc7e7b2ed963228e3edb759f29a4cdeb1eda
timon <k.poloczek7@gmail.com>
2022-04-30 19:36:19 +0200

    add rubocop corrections

--- app/controllers/admin_users/kudos_controller.rb
+++ app/controllers/admin_users/kudos_controller.rb
@@ -1,6 +1,6 @@
-module AdminUsers  
+module AdminUsers
   class KudosController < AdminUserController
-    before_action :set_kudo, only: %i[:show, :destroy]
+    before_action :set_kudo, only: %i[show destroy]
     before_action :authenticate_admin_user!
 
     # GET /admin_users/kudos
@@ -9,27 +9,25 @@ module AdminUsers
     end
 
     # GET /admin_users/kudos/1
-    def show
-    end
+    def show; end
 
     # DELETE /admin_users/kudos/1
     def destroy
       @kudo = Kudo.find(params[:id])
-    if @kudo.present?
-      @kudo.destroy
-    end
+      @kudo.destroy if @kudo.present?
       redirect_to admin_users_kudos_url, notice: 'Kudo was successfully destroyed.'
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_kudo
-        @kudo = Kudo.find(params[:id])
-      end
 
-      # Only allow a list of trusted parameters through.
-      def kudo_params
-        params.require(:kudo).permit(:title, :content, :giver_id, :receiver_id)
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_kudo
+      @kudo = Kudo.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def kudo_params
+      params.require(:kudo).permit(:title, :content, :giver_id, :receiver_id)
+    end
   end
 end
