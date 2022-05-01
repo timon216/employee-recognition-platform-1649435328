9279c2498357838d4d9950d32eabb745d22bab53
timon <k.poloczek7@gmail.com>
2022-05-01 15:09:05 +0200

    add column for employee's  available kudos

---
+++ db/migrate/20220501130502_add_number_of_available_kudos_to_employees.rb
@@ -0,0 +1,5 @@
+class AddNumberOfAvailableKudosToEmployees < ActiveRecord::Migration[6.1]
+  def change
+    add_column :employees, :number_of_available_kudos, :integer, null: false, default: 10
+  end
+end
