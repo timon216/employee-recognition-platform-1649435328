058b554dabcdfdbc73ca3d37324cef3f8740495f
timon <k.poloczek7@gmail.com>
2022-05-03 16:05:03 +0200

    add rubocop corrections

--- app/controllers/admin_users/employees_controller.rb
+++ app/controllers/admin_users/employees_controller.rb
@@ -1,6 +1,6 @@
 module AdminUsers
   class EmployeesController < ApplicationController
-    before_action :set_employee, only: [:show, :edit, :update, :destroy]
+    before_action :set_employee, only: %i[show edit update destroy]
     before_action :authenticate_admin_user!
 
     # GET /admin_users/employees
@@ -9,19 +9,15 @@ module AdminUsers
     end
 
     # GET /admin_users/employees/1
-    def show
-    end
+    def show; end
 
     # GET /admin_users/employees/1/edit
-    def edit
-    end
+    def edit; end
 
     # PATCH/PUT /admin_users/employees/1
     def update
       if employee_params[:password].blank?
-        if !@employee.update(employee_params.except(:password))
-          render :edit          
-        end
+        render :edit unless @employee.update(employee_params.except(:password))
         redirect_to admin_users_employees_path, notice: 'Employee was successfully updated.'
       elsif @employee.update(employee_params)
         redirect_to admin_users_employees_path, notice: 'Employee was successfully updated.'
@@ -37,14 +33,15 @@ module AdminUsers
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_employee
-        @employee = Employee.find(params[:id])
-      end
 
-      # Only allow a list of trusted parameters through.
-      def employee_params
-        params.require(:employee).permit(:email, :password, :number_of_available_kudos)
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_employee
+      @employee = Employee.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def employee_params
+      params.require(:employee).permit(:email, :password, :number_of_available_kudos)
+    end
   end
 end
