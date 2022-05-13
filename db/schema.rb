6331df2c4c75a377111ad1c0481c5bfebab6e222
timon <k.poloczek7@gmail.com>
2022-05-13 23:48:41 +0200

    fix CompanyValues - add unique index for title column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_12_184525) do
+ActiveRecord::Schema.define(version: 2022_05_13_214427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2022_05_12_184525) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_company_values_on_title", unique: true
   end
 
   create_table "employees", force: :cascade do |t|
