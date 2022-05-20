4b58925a4addf60609b8af622ffc905dc4d39938
timon <k.poloczek7@gmail.com>
2022-05-20 23:49:59 +0200

    refactor - avoid callbacks in admin's employees  controller

--- app/controllers/admin_users/employees_controller.rb
+++ app/controllers/admin_users/employees_controller.rb
@@ -1,6 +1,5 @@
 module AdminUsers
   class EmployeesController < ApplicationController
-    before_action :set_employee, only: %i[show edit update destroy]
     before_action :authenticate_admin_user!
 
     # GET /admin_users/employees
@@ -9,13 +8,18 @@ module AdminUsers
     end
 
     # GET /admin_users/employees/1
-    def show; end
+    def show
+      employee
+    end
 
     # GET /admin_users/employees/1/edit
-    def edit; end
+    def edit
+      employee
+    end
 
     # PATCH/PUT /admin_users/employees/1
     def update
+      employee
       if employee_params[:password].blank?
         render :edit unless @employee.update(employee_params.except(:password))
         redirect_to admin_users_employees_path, notice: 'Employee was successfully updated.'
@@ -28,15 +32,15 @@ module AdminUsers
 
     # DELETE /admin_users/employees/1
     def destroy
-      @employee.destroy
+      employee.destroy
       redirect_to admin_users_employees_url, notice: 'Employee was successfully destroyed.'
     end
 
     private
 
     # Use callbacks to share common setup or constraints between actions.
-    def set_employee
-      @employee = Employee.find(params[:id])
+    def employee
+      @employee ||= Employee.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
