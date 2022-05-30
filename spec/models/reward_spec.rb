d023a65cb273aed8bdd0125391718d72c4c34f14
timon <k.poloczek7@gmail.com>
2022-05-30 22:42:45 +0200

    update Reward's model spec

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Reward, type: :model do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:price) }
-    it { is_expected.to validate_numericality_of(:price) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
   end
 end
