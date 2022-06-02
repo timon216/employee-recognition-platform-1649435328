31d33105b5f736161ce96cdb4a7c61c39e248f2d
timon <k.poloczek7@gmail.com>
2022-06-02 23:29:05 +0200

    delete unnecessary files

--- app/controllers/admin_users/admin_user_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module AdminUsers
-  class AdminUserController < ApplicationController
-    before_action :authenticate_admin_user!
-
-    layout 'admin_user'
-  end
-end
