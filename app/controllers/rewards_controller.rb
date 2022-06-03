6e0d791e63cfcd3678d48c913556646d7ed7eaae
timon <k.poloczek7@gmail.com>
2022-06-03 21:30:28 +0200

    add Reward's controller

---
+++ app/controllers/rewards_controller.rb
@@ -0,0 +1,22 @@
+class RewardsController < ApplicationController
+    before_action :authenticate_employee!
+  
+    def index
+      @rewards = Reward.all
+    end
+  
+    def show
+      reward
+    end
+
+    private
+  
+    def reward
+      @reward ||= Reward.find(params[:id])
+    end
+
+    def reward_params
+      params.require(:reward).permit(:title, :description, :price)
+    end
+  end
+  
