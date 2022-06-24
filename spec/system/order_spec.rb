54694c309589f2b18654b6284f3e03c7eb8447b3
timon <k.poloczek7@gmail.com>
2022-06-24 20:12:54 +0200

    update Order's spec - use better specs

--- spec/system/order_spec.rb
+++ spec/system/order_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Order - buying a Reward', type: :system do
   let!(:kudo) { create(:kudo, receiver: employee1) }
   let!(:reward1) { create(:reward, price: 1) }
 
-  context 'when I buy a Reward' do
+  context 'when an Employee buys a Reward' do
     it 'lowers number of points after purchase' do
       sign_in employee1
       visit rewards_path
