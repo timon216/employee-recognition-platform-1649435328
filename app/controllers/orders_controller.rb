08e733243bfd2ce77545ef09db2c5c857c62d099
timon <k.poloczek7@gmail.com>
2022-07-12 21:06:06 +0200

    add index method and update create method in Order's controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,15 @@
 class OrdersController < ApplicationController
   before_action :authenticate_employee!
 
+  def index 
+    @orders = Order.where(employee: current_employee)
+  end
+
   def create
     if current_employee.earned_points < reward.price
       redirect_to rewards_path, notice: "You don't have enough points to buy this reward"
     else
-      order = Order.new(employee: current_employee, reward: reward)
+      order = Order.new(employee: current_employee, reward: reward, reward_snapshot: reward)
       order.save
       redirect_to rewards_path, notice: 'You have bought a new reward'
     end
