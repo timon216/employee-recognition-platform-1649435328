b66a15f8c71a99c1a49b89d2d07a24649a92e05e
timon <k.poloczek7@gmail.com>
2022-07-16 20:14:56 +0200

    add rubocop corrections

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,16 +1,15 @@
 module Admins
-    class OrdersController < AdminController
-      before_action :authenticate_admin!
-  
-      def index
-        @orders = Order.includes(:employee).all
-      end
-  
-      private
-  
-      def employee
-        @employee ||= Employee.find(params[:employee_id])
-      end
+  class OrdersController < AdminController
+    before_action :authenticate_admin!
+
+    def index
+      @orders = Order.includes(:employee).all
     end
+
+    private
+
+    def employee
+      @employee ||= Employee.find(params[:employee_id])
+    end
+  end
 end
-  
--- spec/system/admin_orders_spec.rb
+++ spec/system/admin_orders_spec.rb
@@ -9,23 +9,21 @@ RSpec.describe 'Admin - listing Employees\' Orders', type: :system do
   let!(:employee) { create(:employee) }
   let!(:order) { create(:order, employee: employee) }
 
-  before do
-    sign_in admin
-    visit '/admins/pages/dashboard'
-  end
+  context 'when Admin lists Orders' do
+    before do
+      sign_in admin
+      visit '/admins/pages/dashboard'
+    end
 
-  context 'when Admin lists all Orders' do
-    it 'shows all Orders' do
+    it 'shows Orders of all Employees' do
       click_link 'Orders'
       expect(page).to have_content order.reward_snapshot.title
       expect(page).to have_content order.reward_snapshot.description
       expect(page).to have_content order.created_at.strftime('%d/%m/%Y')
       expect(page).to have_content order.purchase_price
     end
-  end
 
-  context 'when Admin lists one Employee\'s Orders' do
-    it 'shows all Employee\'s Orders' do
+    it 'shows Orders of each Employee in their details\' page' do
       click_link 'Employees'
       click_link 'Show details'
       expect(page).to have_content employee.email
