bf9cecc9dd72e13306e1a7bf3440fb9aeb9bb571
timon <k.poloczek7@gmail.com>
2022-07-16 15:31:59 +0200

    add Orders controller to Admin

---
+++ app/controllers/admins/orders_controller.rb
@@ -0,0 +1,16 @@
+module Admins
+    class OrdersController < AdminController
+      before_action :authenticate_admin!
+  
+      def index
+        @orders = Order.includes(:employee).all
+      end
+  
+      private
+  
+      def employee
+        @employee ||= Employee.find(params[:employee_id])
+      end
+    end
+end
+  
